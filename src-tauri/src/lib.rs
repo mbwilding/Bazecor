f532de19c8684b0049a6da1ed86c7d0fef4657d8
Matthew Wilding <mbwilding@gmail.com>
2025-03-05 01:33:24 +0800

    Tauri init

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,124 @@
+use anyhow::anyhow;
+use dygma_focus::prelude::*;
+use std::sync::Mutex;
+use tauri::{Result, State};
+
+struct Storage {
+    focus: Mutex<Option<Focus>>,
+}
+
+#[tauri::command]
+fn find_all_devices() -> Result<Vec<Device>> {
+    Ok(Focus::find_all_devices()?
+        .into_iter()
+        // For macOS as it will return 2 serial devices per dygma device
+        .filter(|device| !device.serial_port.starts_with("/dev/cu."))
+        .collect())
+}
+
+#[tauri::command]
+fn connect(port: &str, storage: State<Storage>) -> Result<()> {
+    *storage.focus.lock().unwrap() = Some(Focus::new_via_port(port)?);
+    Ok(())
+}
+
+#[tauri::command]
+fn disconnect(storage: State<Storage>) {
+    *storage.focus.lock().unwrap() = None;
+}
+
+fn with_focus<T, F>(storage: State<Storage>, func: F) -> Result<T>
+where
+    F: FnOnce(&mut Focus) -> Result<T>,
+{
+    let mut focus_guard = storage.focus.lock().unwrap();
+    let focus = focus_guard
+        .as_mut()
+        .ok_or_else(|| anyhow!("Not connected"))?;
+    match func(focus) {
+        Ok(result) => Ok(result),
+        Err(err) => {
+            *focus_guard = None;
+            Err(err)
+        }
+    }
+}
+
+#[tauri::command]
+fn version(storage: State<Storage>) -> Result<String> {
+    with_focus(storage, |focus| Ok(focus.version()?))
+}
+
+#[tauri::command]
+fn settings_get(storage: State<Storage>) -> Result<Settings> {
+    with_focus(storage, |focus| Ok(focus.settings_get()?))
+}
+
+#[tauri::command]
+fn settings_set(data: Settings, storage: State<Storage>) -> Result<()> {
+    with_focus(storage, |focus| Ok(focus.settings_set(&data)?))
+}
+
+#[tauri::command]
+fn palette_rgb_get(storage: State<Storage>) -> Result<Vec<RGB>> {
+    with_focus(storage, |focus| Ok(focus.palette_rgb_get()?))
+}
+
+#[tauri::command]
+fn palette_rgb_set(data: Vec<RGB>, storage: State<Storage>) -> Result<()> {
+    with_focus(storage, |focus| Ok(focus.palette_rgb_set(&data)?))
+}
+
+#[tauri::command]
+fn palette_rgbw_get(storage: State<Storage>) -> Result<Vec<RGBW>> {
+    with_focus(storage, |focus| Ok(focus.palette_rgbw_get()?))
+}
+
+#[tauri::command]
+fn palette_rgbw_set(data: Vec<RGBW>, storage: State<Storage>) -> Result<()> {
+    with_focus(storage, |focus| Ok(focus.palette_rgbw_set(&data)?))
+}
+
+#[tauri::command]
+fn color_map_get(storage: State<Storage>) -> Result<Vec<u8>> {
+    with_focus(storage, |focus| Ok(focus.color_map_get()?))
+}
+
+#[tauri::command]
+fn color_map_set(data: Vec<u8>, storage: State<Storage>) -> Result<()> {
+    with_focus(storage, |focus| Ok(focus.color_map_set(&data)?))
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .format(|out, message, record| {
+                    let tech = if record.target() == "bazecor_lib" { "B" } else { "F" };
+                    out.finish(format_args!("[{}] {}: {}", record.level(), tech, message))
+                })
+                .level(log::LevelFilter::Debug)
+                .build(),
+        )
+        .plugin(tauri_plugin_opener::init())
+        .manage(Storage {
+            focus: Default::default(),
+        })
+        .invoke_handler(tauri::generate_handler![
+            find_all_devices,
+            connect,
+            disconnect,
+            version,
+            settings_get,
+            settings_set,
+            palette_rgb_get,
+            palette_rgb_set,
+            palette_rgbw_get,
+            palette_rgbw_set,
+            color_map_get,
+            color_map_set,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
--- vite.config.mts
+++ vite.config.mts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vitest/config";
 import { resolve } from 'path'
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
 	plugins: [],
 	root: 'src',
@@ -23,5 +25,36 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
-	}
+	},
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  // to access the Tauri environment variables set by the CLI with information about the current target
+  envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
 });
