d59bf395da21eefb25fd53f08919e392330ce185
Matthew Wilding <mbwilding@gmail.com>
2025-03-06 01:32:01 +0800

    colored logging in release

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -103,6 +103,7 @@ pub fn run() {
 
     #[cfg(not(debug_assertions))]
     {
+        use tauri_plugin_log::fern::colors::ColoredLevelConfig;
         use tauri_plugin_log::{Builder, Target, TargetKind};
 
         let log_plugin = Builder::default()
@@ -111,6 +112,7 @@ pub fn run() {
                 Target::new(TargetKind::LogDir { file_name: None }),
                 Target::new(TargetKind::Webview),
             ])
+            .with_colors(ColoredLevelConfig::default())
             .build();
 
         builder = builder.plugin(log_plugin);
