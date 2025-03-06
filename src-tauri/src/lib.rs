ce6f4fe37cc411d0925f8981ed68b7a6b5f6338e
Matthew Wilding <mbwilding@gmail.com>
2025-03-06 14:13:01 +0800

    Rust clean up

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,147 +1,31 @@
-use anyhow::anyhow;
+mod commands;
+mod helpers;
+
+use crate::commands::*;
+use crate::helpers::*;
 use dygma_focus::prelude::*;
-use log::debug;
-use serde_json::json;
-use std::collections::HashMap;
 use std::sync::Mutex;
-use tauri::{App, Result, State};
-use tauri_plugin_store::StoreExt;
 
 struct Storage {
     focus: Mutex<Option<Focus>>,
-}
-
-#[tauri::command]
-fn find_all_devices() -> Result<Vec<Device>> {
-    Ok(Focus::find_all_devices()?
-        .into_iter()
-        // For macOS as it will return 2 serial devices per dygma device
-        .filter(|device| !device.serial_port.starts_with("/dev/cu."))
-        .collect())
-}
-
-#[tauri::command]
-fn connect(port: &str, storage: State<Storage>) -> Result<()> {
-    *storage.focus.lock().unwrap() = Some(Focus::new_via_port(port)?);
-    Ok(())
-}
-
-#[tauri::command]
-fn disconnect(storage: State<Storage>) {
-    *storage.focus.lock().unwrap() = None;
-}
-
-fn with_focus<T, F>(storage: State<Storage>, func: F) -> Result<T>
-where
-    F: FnOnce(&mut Focus) -> Result<T>,
-{
-    let mut focus_guard = storage.focus.lock().unwrap();
-    let focus = focus_guard
-        .as_mut()
-        .ok_or_else(|| anyhow!("Not connected"))?;
-    match func(focus) {
-        Ok(result) => Ok(result),
-        Err(err) => {
-            *focus_guard = None;
-            Err(err)
-        }
-    }
-}
-
-#[tauri::command]
-fn version(storage: State<Storage>) -> Result<String> {
-    with_focus(storage, |focus| Ok(focus.version()?))
-}
-
-#[tauri::command]
-fn settings_get(storage: State<Storage>) -> Result<Settings> {
-    with_focus(storage, |focus| Ok(focus.settings_get()?))
-}
-
-#[tauri::command]
-fn settings_set(data: Settings, storage: State<Storage>) -> Result<()> {
-    with_focus(storage, |focus| Ok(focus.settings_set(&data)?))
-}
-
-#[tauri::command]
-fn palette_rgb_get(storage: State<Storage>) -> Result<Vec<RGB>> {
-    with_focus(storage, |focus| Ok(focus.palette_rgb_get()?))
-}
-
-#[tauri::command]
-fn palette_rgb_set(data: Vec<RGB>, storage: State<Storage>) -> Result<()> {
-    with_focus(storage, |focus| Ok(focus.palette_rgb_set(&data)?))
-}
-
-#[tauri::command]
-fn palette_rgbw_get(storage: State<Storage>) -> Result<Vec<RGBW>> {
-    with_focus(storage, |focus| Ok(focus.palette_rgbw_get()?))
-}
-
-#[tauri::command]
-fn palette_rgbw_set(data: Vec<RGBW>, storage: State<Storage>) -> Result<()> {
-    with_focus(storage, |focus| Ok(focus.palette_rgbw_set(&data)?))
-}
-
-#[tauri::command]
-fn color_map_get(storage: State<Storage>) -> Result<Vec<u8>> {
-    with_focus(storage, |focus| Ok(focus.color_map_get()?))
-}
-
-#[tauri::command]
-fn color_map_set(data: Vec<u8>, storage: State<Storage>) -> Result<()> {
-    with_focus(storage, |focus| Ok(focus.color_map_set(&data)?))
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let mut builder = tauri::Builder::default();
 
-    #[cfg(debug_assertions)]
-    {
-        let devtools = tauri_plugin_devtools::init();
-        builder = builder.plugin(devtools);
-    }
-
-    #[cfg(not(debug_assertions))]
-    {
-        use tauri_plugin_log::fern::colors::ColoredLevelConfig;
-        use tauri_plugin_log::{Builder, Target, TargetKind};
-
-        let log_plugin = Builder::default()
-            .targets([
-                Target::new(TargetKind::Stdout),
-                Target::new(TargetKind::LogDir { file_name: None }),
-                Target::new(TargetKind::Webview),
-            ])
-            .with_colors(ColoredLevelConfig::default())
-            .build();
-
-        builder = builder.plugin(log_plugin);
-    }
+    builder = observability(builder);
 
     builder
-        .manage(Storage {
-            focus: Default::default(),
-        })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_os::init())
-        // .plugin(
-        //     tauri_plugin_log::Builder::new()
-        //         .format(|out, message, record| {
-        //             let tech = if record.target() == "bazecor_lib" {
-        //                 "B"
-        //             } else {
-        //                 "F"
-        //             };
-        //             out.finish(format_args!("[{}] {}: {}", record.level(), tech, message))
-        //         })
-        //         // .level(log::LevelFilter::Debug)
-        //         .build(),
-        // )
+        .manage(Storage {
+            focus: Default::default(),
+        })
         .setup(|app| {
             settings(app)?;
+
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
@@ -160,53 +44,4 @@ pub fn run() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-fn settings(app: &mut App) -> anyhow::Result<()> {
-    // Create a new store or load the existing one
-    // this also put the store in the app's resource table
-    // so your following calls `store` calls (from both rust and js)
-    // will reuse the same store
-    //
-    // Directory:
-    //   macOS: ~/Library/Application Support/com.bazecore.app/settings.json
-    //   windows: ~\AppData\Roaming\com.bazecor.app\settings.json
-    let store = app.store("settings.json")?;
-
-    let settings_defaults = HashMap::from([
-        ("settings.backupFolder", json!("")),
-        ("settings.backupFrequency", json!(0)),
-        ("settings.language", json!("english")),
-        ("settings.darkMode", json!("system")),
-        ("settings.hideBluetoothExperimental", json!(false)),
-        ("settings.showDefaults", json!(false)),
-        ("settings.autoUpdate", json!(null)),
-        ("settings.verbose", json!(false)),
-        ("settings.version", json!(null)),
-    ]);
-
-    for (key, default_value) in &settings_defaults {
-        if !store.has(key) {
-            store.set(key.to_string(), default_value.clone());
-        }
-    }
-
-    let settings_keys: Vec<&str> = settings_defaults.keys().cloned().collect();
-
-    #[cfg(debug_assertions)]
-    {
-        debug!("Begin settings dump");
-        for key in settings_keys {
-            let value = store
-                .get(key)
-                .expect(&format!("Failed to get value for key {}", key));
-            debug!("{}: {}", key, value);
-        }
-        debug!("End settings dump");
-    }
-
-    // Remove the store from the resource table
-    // store.close_resource();
-
-    Ok(())
 }
